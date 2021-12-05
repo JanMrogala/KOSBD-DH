4476996e290f2c799a899cc7835eccd541683d9d
JanMrogala <jan.mrogala@seznam.cz>
2021-12-05 01:48:15 +0100

    UI for first step of DH

--- js/indexPageScripts.js
+++ js/indexPageScripts.js
@@ -1,14 +1,3 @@
-$("#submitN").click(function () {
-  let number = parseInt($("#numberN").val());
-  if (Number.isInteger(number)) {
-    let prim = findPrimitive(number);
-    $("#nOutput").text(
-      "Smallest primitive root of " +
-        number +
-        " is " +
-        (prim != -1 ? prim : "none")
-    );
-  } else {
-    alert("Input is not a correct number");
-  }
+$(document).ready(function () {
+  $(".collapsible").collapsible();
 });
--- js/primeNumbers.js
+++ js/primeNumbers.js
@@ -94,3 +94,8 @@ function findPrimitive(n) {
   // If no primitive root found
   return -1;
 }
+
+function isPrime(num) {
+  for (let i = 2; i * i <= num; i++) if (num % i === 0) return false;
+  return num > 1;
+}
